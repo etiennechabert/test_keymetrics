fb6b8d73967afa9d33f750822f885fe524299207
Chabert Etienne <etienne.chabert@gmail.com>
2017-10-10 14:32:08 +0200

    Add todo test for module basic error

--- tests/2-Module.js
+++ tests/2-Module.js
@@ -137,6 +137,14 @@ describe("Module basic", function() {
     });
 });
 
+describe("Module errors case", function() {
+    it("Should handle create module for user 404");
+    it("Should handle a publish update for an unautorized user");
+    it("Should handle a get on a module 404");
+    it("SHould handle a download on a module 404");
+    it("Should handle a download of an existing module but for a version 404")
+});
+
 describe("done", function() {
     it("Exit", () => {
         process.exit(0);
